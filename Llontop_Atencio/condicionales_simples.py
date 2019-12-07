b79c1a2fa9168b189bdf207d971e1709108459cc
Llontop-Atencio08 <57873068+llontop-atencio08@users.noreply.github.com>
2019-12-07 17:04:07 -0500

    se agrego el ejercicio nro 1 de condicionales simples

---
+++ Llontop_Atencio/condicionales_simples.py
@@ -0,0 +1,18 @@
+#programa alumno aprobado
+import os
+#declaracion
+nombre=""
+promedio=0.0
+
+#Input via os
+nombre=os.sys.argv[1]
+promedio=float(os.sys.argv[2])
+
+#procesing
+#si el alumno supera a 11
+#mostrar "felicidades aprobaste"
+if(promedio>11):
+     print(nombre,"felicidades aprobaste")
+
+
+
---
+++ lote.bat
@@ -0,0 +1,5 @@
+python Llontop_Atencio/condicionales_simples.py "saul" 17
+python Llontop_Atencio/condicionales_simples.py "alondra" 15
+python Llontop_Atencio/condicionales_simples.py "santiago" 15
+
+
