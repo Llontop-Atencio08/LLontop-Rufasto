c02c84986f4d5409e5b0da32dd622d5add9089c1
Llontop-Atencio08 <57873068+llontop-atencio08@users.noreply.github.com>
2019-12-09 12:12:27 -0500

    se agrego el ejercicio nro 3 de condicionales simples

--- Llontop_Atencio/condicionales_simples.py
+++ Llontop_Atencio/condicionales_simples.py
@@ -31,3 +31,23 @@ nro_botellas=int(os.sys.argv[2])
 #mostrar "adulto alcoholico"
 if(nro_botellas>50):
     print(nombre_adulto,"adulto alcoholico")
+
+
+#Ejercicio3
+#programa de adolescente para ingreso a discoteca
+import os
+#declaracion
+nombre_adolescente=""
+nombre_discoteca=""
+edad_adolescente=0
+
+#Input via os
+nombre_adolescente=os.sys.argv[1]
+nombre_discoteca=os.sys.argv[2]
+edad_adolescente=int(os.sys.argv[3])
+
+#procesing
+#si la edad supera a los 18 anios
+#mostrar "adulto mayor de edad"
+if(edad_adolescente>18):
+    print(nombre_adolescente,"bienvenido")
--- lote.bat
+++ lote.bat
@@ -6,3 +6,7 @@ python Llontop_Atencio/condicionales_simples.py "santiago" 15
 python Llontop_Atencio/condicionales_simples.py "pedro" 52
 python Llontop_Atencio/condicionales_simples.py "juan" 60
 python Llontop_Atencio/condicionales_simples.py "miguel" 51
+
+python Llontop_Atencio/condicionales_simples.py "jose" "night" 20
+python Llontop_Atencio/condicionales_simples.py "guzman" "night" 19
+python Llontop_Atencio/condicionales_simples.py "fabiola" "night" 21
